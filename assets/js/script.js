43100798e70b6e16842cda7fd9203614eb3225ae
Nicholas Bales <nickb96@icloud.com>
2022-08-09 19:33:14 -0400

    added working code for title page and questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,35 @@
-// GIVEN I am taking a code quiz
-// WHEN I click the start button
-// THEN a timer starts and I am presented with a question
-// WHEN I answer a question
-// THEN I am presented with another question
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-// WHEN the game is over
-// THEN I can save my initials and my score
-
-// Landing page
-  // Start button
-  // Description of app: welcome to quiz, this is how it works
-  // Event listener on start button to begin, listening for clicks
-  // When start is clicked, landing page disappears, first question appears  and time appears, begins counting down
-  // Need timer that counts down
-// Set of questions - array of objects
-  // ? What do questions need to include?
-  // Question text - string
-  // Set of answers - array of strings
-  // Check if answer is correct - give string or index of correct answer 
-  // ! Check if user is correct - does NOT need to be with other question info
-// Need way to display question text, answers, and answers need to be clickable
-// When answer is clicked
-  // Conpares answer chosen to correct answer (if trues)
-  // Displays feedback based on correct or incorrect
-  // If answer is incorrect, subtract time from clock
-  // Regardless of incorrect or correct, move on to next question, UNLESS user is on final question.
-// If user is on last question, once an answer is selected, they are presented with a form to enter their initials 
-  //Initials and remaining time is saved to local storage as score
-// Game ense when all questions are answered OR timer reaches 0 -- is there a case where timer might go lower than 0?
 var questions = document.querySelector("#questions");
 var timerElement = document.querySelector(".timer");
 var startButton = document.querySelector(".start-button");
-var answersDiv = document.querySelector("#answers")
-// console.log(document.querySelector("#answers"));
-var index = 0
+var answersDiv = document.querySelector("#answers");
+var qContainer = document.querySelector(".questionContainer");
+var welcome = document.querySelector("#welcome");
+var endText = document.querySelector("#endText");
+var quesText = document.querySelector("#questionText");
+var index = 0;
 var score;
-var isWin = false;
 var timer;
 var timerCount;
 var chosenQuestion = [];
 
 var questions = [
-  {text:"what defines the initial, basic structure of a webpage?", answers:[{name: "CSS", value: false}, {name: "Javascript", value: false}, {name: "HTML", value: true}, {name: "C++", value: false}]}
+  { text: "What defines the initial, basic structure of a webpage?", answers: [{ name: "CSS", value: false }, { name: "Javascript", value: false }, { name: "HTML", value: true }, { name: "C++", value: false }] },
+  { text: "What does DOM stand for?", answers: [{ name: "Document Object Model", value: true }, { name: "Data Orientation Manager", value: false }, { name: "Danger Overload Method", value: false }, { name: "Dub On Butt", value: false }] },
+  { text: "What defines the initial, basic structure of a webpage?", answers: [{ name: "CSS", value: false }, { name: "Javascript", value: false }, { name: "HTML", value: true }, { name: "C++", value: false }] },
+  { text: "What does DOM stand for?", answers: [{ name: "Document Object Model", value: true }, { name: "Data Orientation Manager", value: false }, { name: "Danger Overload Method", value: false }, { name: "Dub On Butt", value: false }] },
+  { text: "What defines the initial, basic structure of a webpage?", answers: [{ name: "CSS", value: false }, { name: "Javascript", value: false }, { name: "HTML", value: true }, { name: "C++", value: false }] },
+  { text: "What does DOM stand for?", answers: [{ name: "Document Object Model", value: true }, { name: "Data Orientation Manager", value: false }, { name: "Danger Overload Method", value: false }, { name: "Dub On Butt", value: false }] },
+  { text: "What defines the initial, basic structure of a webpage?", answers: [{ name: "CSS", value: false }, { name: "Javascript", value: false }, { name: "HTML", value: true }, { name: "C++", value: false }] },
+  { text: "What does DOM stand for?", answers: [{ name: "Document Object Model", value: true }, { name: "Data Orientation Manager", value: false }, { name: "Danger Overload Method", value: false }, { name: "Dub On Butt", value: false }] },
+  { text: "What defines the initial, basic structure of a webpage?", answers: [{ name: "CSS", value: false }, { name: "Javascript", value: false }, { name: "HTML", value: true }, { name: "C++", value: false }] },
+  { text: "What does DOM stand for?", answers: [{ name: "Document Object Model", value: true }, { name: "Data Orientation Manager", value: false }, { name: "Danger Overload Method", value: false }, { name: "Dub On Butt", value: false }] },
 ]
 
 function startGame() {
-  console.log("Game started");
-  isWin = false;
   timerCount = 60;
-  startButton.disabled = true;
+  qContainer.classList.remove("hide");
+  welcome.classList.add("hide");
+  startButton.classList.add("hide");
   startTimer()
   renderQuestions()
 }
@@ -63,115 +41,54 @@ function loseGame() {
   setLosses()
 }
 
-// function endGame() {
-
-// }
-
-function startTimer () {
-  console.log("Made it to timer");
-  timer = setInterval(function() {
+function startTimer() {
+  timer = setInterval(function () {
     timerCount--;
     timerElement.textContent = timerCount;
-    if (timerCount >= 0) {
-      if (isWin && timerCount > 0) {
-        clearInterval(timer)
-      } if (timerCount === 0) {
-        clearInterval(timer)
-      }
+    if (timerCount <= 0) {
+      endGame()
     }
   }, 1000);
 }
 var index = 0
 
 function renderQuestions() {
-  console.log("You got to your questions");
-  document.getElementById("#questions").text= questions[index];
-
-  answersDiv.innerHTML='';
-
+  document.getElementById("questions").innerHTML = questions[index].text;
+  answersDiv.innerHTML = '';
   questions[index].answers.forEach(answer => {
     var button = document.createElement("button")
-    button.setAttribute("value", answer.value)
+    button.setAttribute("data-value", answer.value)
     button.textContent = answer.name
-    answersDiv.appendChild("button")
-    button.onclick = function() {
-      var correct = answer.value
-      if (guess === true) {
+    answersDiv.appendChild(button)
+    button.onclick = function (event) {
+      var guess = event.target.dataset.value
+      console.log(guess);
+      if (guess === "true") {
         console.log("correct");
-        score += 1
       } else {
         console.log("Incorrect");
-        timerCount -= 8
+        timerCount -= 5
       }
       index++
-      renderQuestions()
-    }
-    if (timerCount === 0) {
-      endGame();
+      if (questions.length > index) {
+        renderQuestions()
+      } else {
+        endGame()
+      }
     }
   });
 }
 
-startButton.addEventListener("click", startGame);
+function endGame() {
+  clearInterval(timer);
+  qContainer.classList.add("hide");
+  endText.classList.remove("hide");
+  // display form
 
-//   { 
-//     number: "question1",
-//     question: "What defines the initial, basic structure of a webpage?",
-//     choices: ["CSS", "JavaScript", "HTML", "C++"],
-//     answer: "HTML",
-//   },
-//   {
-//     number: "question2",
-//     question: "What does DOM stand for?",
-//     choices: ["Document Object Model", "Data Orientation Manager", "Danger Overload Method", "Dune Overthrown by Muad'Dib"],
-//     answer: "Document Object Model",
-//   },
-//   {
-//     number: "question3",
-//     question: "What is a variable number within a JavaScript object called?",
-//     choices: ["Function", "Key", "Method", "Limiter"],
-//     answer: "Key",
-//   },
-//   {
-//     number: "question4",
-//     question: "What does most of the styling on a webpage?",
-//     choices: ["HTML", "jQuery", "JavaScript", "CSS"],
-//     answer: "CSS",
-//   },
-//   {
-//     number: "question5",
-//     question: "What is a JavaScript method?",
-//     choices: ["A style of coding", "An approach to acting", "A function within an object", "A discrete bit of code that performs an action"],
-//     answer: "A function within an object",
-//   },
-//   {
-//     number: "question6",
-//     question: "What does it mean to 'call' a function?",
-//     choices: ["Use the word 'function'", "Define the action the function will take", "Give the function a number as part of its definition", "Activate the function by naming it outside of its definition"],
-//     answer: "Activate the function by naming it outside of its definition",
-//   },
-//   {
-//     number: "question7",
-//     question: "Which of the following all use curly brackets {} in JavaScript?",
-//     choices: ["Variables, arrays, functions", "If/else statements, variables, objects", "Arrays, if/else statements, objects", "Functions, objects, if/else statements"],
-//     answer: "Functions, objects, if/else statements",
-//   },
-//   {
-//     number: "question8",
-//     question: "Which of the following uses square brackets [] in JavaScript?",
-//     choices: ["Objects", "Arrays", "Functions", "Variables"],
-//     answer: "Arrays",
-//   },
-//   {
-//     number: "question9",
-//     question: "What is a 'child element'?",
-//     choices: ["An element that throws an error", "An element contained directly inside of its parent, with no other containing elements in between", "An element with the capacity to learn", "An element that breaks the code"],
-//     answer: "An element contained directly inside of its parent, with no other containing elements in between",
-//   },
-//   {
-//     number: "question10",
-//     question: "Which of the following are all examples of HTML tags?",
-//     choices: ["<p>, <div>, <main>, <header>", "<h1>, <ul>, <contain>, <hero>", "<img>, <start>, <switch>, <table>", "<engage>, <make-it-so>, <override>, <captain>"],
-//     answer: "<p>, <div>, <main>, <header>",
-//   }
-// }
+  // submit form event click, grab score and user input, save to storage
+  // send to high score page
+  // grab localstorage
+  // play again button
+}
+
+startButton.addEventListener("click", startGame);
